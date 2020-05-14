653239bb7d077643fd23a0a399b14b34c830b5d0
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-14 18:41:14 +0530

    downgraded version of react navigation

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import { View, Text } from "react-native";
 import Signin from "./src/screens/Signin";
 import Signup from "./src/screens/Signup";
@@ -6,50 +7,27 @@ import Account from "./src/screens/Account";
 
 import TrackCreate from "./src/screens/TrackCreate";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator,
+  createSwitchNavigator,
+} from "react-navigation";
 import TrackList from "./src/screens/TrackList";
 import TrackDetails from "./src/screens/TrackDetails";
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: Signup,
+    Signin: Signin,
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackList,
+      TrackDetails: TrackDetails,
+    }),
+    TrackCreate: TrackCreate,
+    Account: Account,
+  }),
+});
 
-const Stack = createStackNavigator();
-
-const Tab = createBottomTabNavigator();
-const isLogin = true;
-function Home() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={TrackList} />
-      <Stack.Screen name="Profile" component={TrackDetails} />
-    </Stack.Navigator>
-  );
-}
-
-function Optional() {
-  if (!isLogin) {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Signup" component={Signup} />
-          <Stack.Screen name="Signin" component={Signin} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  } else {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Messages" component={Home} />
-          <Tab.Screen name="Feed" component={Account} />
-
-          <Tab.Screen name="ggg" component={TrackCreate} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
-
-function App() {
-  return <Optional />;
-}
-export default App;
+export default createAppContainer(switchNavigator);
--- src/screens/TrackList.js
+++ src/screens/TrackList.js
@@ -1,9 +1,6 @@
 import React from "react";
 import TrackDetails from "./TrackDetails";
 import { Text, StyleSheet, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-const Stack = createStackNavigator();
 
 function TrackList({ navigation }) {
   return (
